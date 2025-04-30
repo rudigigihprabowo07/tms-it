77ac93e056c4052c24f8b946590d1622ed000e37
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-04-30 11:58:43 +0700

    feat: crud operation for asset-type use restful API

---
+++ app/Models/AssetTypeModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AssetTypeModel extends Model
+{
+    protected $table            = 'asset_type';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['name', 'description'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'name' => 'required|max_length[100]',
+        'description' => 'required',
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
