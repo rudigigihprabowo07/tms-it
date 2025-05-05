312f6d4aacfa39a7a91cf84f38cb7f9f8aef5b5c
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-05-05 15:03:37 +0700

    create standard asset model

---
+++ app/Models/StandardAssetModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StandardAssetModel extends Model
+{
+    protected $table            = 'standard_asset';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['category_asset_id', 'brand_asset_id', 'name', 'quantity'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'category_asset_id' => 'required|is_natural_no_zero',
+        'brand_asset_id' => 'required|is_natural_no_zero',
+        'name' => 'required|max_length[100]',
+        'quantity' => 'required|is_natural_no_zero'
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
