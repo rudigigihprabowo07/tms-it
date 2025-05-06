e8d72670167fc50c1ecc2088bc3e250792377142
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-05-06 13:35:55 +0700

    migration create rekanan table

---
+++ app/Database/Migrations/2025-05-06-062532_CreateRekananTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateRekananTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => '5',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => false,
+            ],
+            'address' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'deleted_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('rekanan');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('rekanan');
+    }
+}
