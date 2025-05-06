64724abb5dfd9e9a8a5eb39e48d30bd94017e5f4
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-05-06 12:01:33 +0700

    standard asset table join to brand asset and category asset table

--- app/Models/StandardAssetModel.php
+++ app/Models/StandardAssetModel.php
@@ -48,4 +48,27 @@ class StandardAssetModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function getStandardAsset()
+    {
+        $builder = $this->db->table('standard_asset');
+        $builder->select('standard_asset.id as standard_asset_id, category_asset.id as category_asset_id, category_asset.name as category_asset_name, brand_asset.id as brand_asset_id,brand_asset.name as brand_asset_name,standard_asset.name as standard_asset_name, quantity');
+        $builder->join('category_asset', 'standard_asset.category_asset_id = category_asset.id');
+        $builder->join('brand_asset', 'standard_asset.brand_asset_id = brand_asset.id');
+        $builder->where('standard_asset.deleted_at', null);
+        $query = $builder->orderBy('standard_asset_id', 'DESC')->get();
+        return $query->getResult();
+    }
+
+    public function getSpecificStandardAsset($id)
+    {
+        $builder = $this->db->table('standard_asset');
+        $builder->select('standard_asset.id as standard_asset_id, category_asset.id as category_asset_id, category_asset.name as category_asset_name, brand_asset.id as brand_asset_id,brand_asset.name as brand_asset_name,standard_asset.name as standard_asset_name, quantity');
+        $builder->join('category_asset', 'standard_asset.category_asset_id = category_asset.id');
+        $builder->join('brand_asset', 'standard_asset.brand_asset_id = brand_asset.id');
+        $builder->where('standard_asset.id', $id);
+        $builder->where('standard_asset.deleted_at', null);
+        $query = $builder->orderBy('standard_asset_id', 'DESC')->get();
+        return $query->getResult();
+    }
 }
