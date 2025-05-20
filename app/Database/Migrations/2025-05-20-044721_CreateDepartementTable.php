8b29287dba75247af2653286efe97fccc0b62fd0
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-05-20 11:53:10 +0700

    migration: create departement table

---
+++ app/Database/Migrations/2025-05-20-044721_CreateDepartementTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateDepartementTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => '5',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'departement_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => false,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'deleted_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('departement');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('departement');
+    }
+}
