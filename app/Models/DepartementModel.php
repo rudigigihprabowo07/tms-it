7203ff5a719f29a8cd9513e69de788f8803bed95
Rudi Gigih Prabowo <rudi.gigihprabowo@gmail.com>
2025-05-20 11:57:26 +0700

    model: for departement table

---
+++ app/Models/DepartementModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class DepartementModel extends Model
+{
+    protected $table            = 'departement';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['departement_name', 'description'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'departement_name' => 'required|max_length[100]',
+        'description' => 'required',
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
